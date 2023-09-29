3ba4d011c853674cdb55cdb940fc34cfa3eb7e0b
David Moreno <david.moreno@arisnova.com>
2023-09-29 19:48:34 +0200

    new test, with no files and with more than 2 files

--- src/components/common/input/input-file/input-file.test.tsx
+++ src/components/common/input/input-file/input-file.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, test, vi } from "vitest";
+import { beforeEach, describe, expect, test, vi } from "vitest";
 import { fireEvent, render, screen } from "@testing-library/react";
 
 import { InputFile } from "./input-file";
@@ -9,6 +9,10 @@ describe("<InputFile/>", () => {
 
   const testName = "test-input-name";
   const error = "Test error";
+
+  beforeEach(() => {
+    mockOnChange.mockClear();
+  });
 
   test("should render input", () => {
     render(<InputFile name={testName} onChangeInput={mockOnChange} />);
@@ -76,5 +80,46 @@ describe("<InputFile/>", () => {
     fireEvent.change(input, { target: { files: file } });
 
     expect(mockOnChange).toBeCalledTimes(1);
+  });
+
+  test("should call onChange with no file", () => {
+    render(
+      <InputFile
+        name={testName}
+        error={error}
+        onChangeInput={mockOnChange}
+        onBlur={mockOnBlur}
+      />
+    );
+    const input = screen.getByLabelText(testName);
+    fireEvent.change(input, { target: { files: [] } });
+
+    expect(mockOnChange).toBeCalledTimes(0);
+  });
+
+  test("should call with error when upload more than 2 files", () => {
+    render(
+      <InputFile
+        name={testName}
+        error={error}
+        onChangeInput={mockOnChange}
+        onBlur={mockOnBlur}
+      />
+    );
+    const file = new File(["(⌐□_□)"], "chucknorris.png", { type: "image/png" });
+    const file2 = new File(["(⌐□_□)"], "chucknorris.png", {
+      type: "image/png",
+    });
+    const file3 = new File(["(⌐□_□)"], "chucknorris.png", {
+      type: "image/png",
+    });
+
+    const input = screen.getByLabelText(testName);
+    fireEvent.change(input, { target: { files: [file, file2, file3] } });
+
+    expect(mockOnChange).toBeCalledWith(
+      undefined,
+      "You can send a maximum of two files."
+    );
   });
 });
