5b28a7a23261637d4fa3af2a2176af9357766a37
David Moreno <david.moreno@arisnova.com>
2023-09-29 19:26:49 +0200

    fix in app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,20 @@
 import { describe, test } from "vitest";
+import { BrowserRouter } from "react-router-dom";
 
 import { render } from "@testing-library/react";
 
 import { App } from "./App";
+import { Provider } from "react-redux";
+import { store } from "./state/store";
 
 describe("<App/>", () => {
   test("should render App", () => {
-    render(<App />);
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    );
   });
 });
