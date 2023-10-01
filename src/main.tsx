9f3f0a0edac54b56fab9627b520a9bbc80834fef
David Moreno <david.moreno@arisnova.com>
2023-10-01 02:48:43 +0200

    Night inspiration. Finally webcomponent working.

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,7 @@
-import { createRoot } from "react-dom/client";
 import r2wc from "@r2wc/react-to-web-component";
-import { BrowserRouter } from "react-router-dom";
-
 import { App } from "./App";
-import { Provider } from "react-redux";
 
-import { store } from "./state/store";
-
+// eslint-disable-next-line react-refresh/only-export-components
 const WizardMF = r2wc(App);
 
 customElements.define("wizard-mf", WizardMF);
-
-createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-);
